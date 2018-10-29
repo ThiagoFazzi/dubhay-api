06ba62c19a234486fdba569ae851528e249d9e40
ThiagoFazzi <thiago@xtwo.com.br>
2018-10-29 15:20:48 +0100

    express

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express'
+var express = require('express')
+var app = express()
 
-const app = express()
 const port = 8080
 
-app.get('/', (res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage' + req)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- target/index.js
+++ target/index.js
@@ -1,8 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const app = express_1.default();
+var express = require('express');
+var app = express();
 const port = 8080;
-app.get('/', (res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 //# sourceMappingURL=index.js.map
